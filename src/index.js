d7e0174bbed188d4472727cb955a5f4c5091bc0d
Waris Ansar <bhehzadprince@gmail.com>
2024-06-18 15:24:47 +0500

    response and error configutation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log(err, "error running server");
+      throw errror;
+    });
+    app.listen(
+      process.env.PORT || 8000,
+      console.log("server is runnin at the port 8000")
+    );
+  })
+  .catch((err) => console.log("connection failed", err));
 
 // (async () => {
 //   try {
